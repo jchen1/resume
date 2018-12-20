269441e62253c769b1f9d34ee3161a94dae8fda3
Jeff Chen <hello@jeff.yt>
2018-12-20 12:01:45 -0600

    clean up deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,30 @@
-// Include gulp
 var gulp = require('gulp');
 
-// Include Our Plugins
-// var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
+var gulpSass = require('gulp-sass');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
 var clean = require('gulp-clean-css');
-var uncss = require('gulp-uncss');
 var autoprefixer = require('gulp-autoprefixer');
 
-// Lint Task
-// gulp.task('lint', function() {
-//     return gulp.src('js/*.js')
-//         .pipe(jshint())
-//         .pipe(jshint.reporter('default'));
-// });
-
 // Compile Our Sass
-gulp.task('sass', function() {
+function sass() {
     return gulp.src('scss/*.scss')
-        .pipe(sass())
+        .pipe(gulpSass())
         .pipe(autoprefixer({browsers:['> 1%'], cascade: false}))
         .pipe(gulp.dest('css'));
-});
+}
 
-gulp.task('css', function() {
+function css() {
     return gulp.src('css/*.css')
-        // .pipe(uncss({html:['index.html']}))
         .pipe(clean({compatibility: 'ie8'}))
         .pipe(concat('resume.min.css'))
         .pipe(gulp.dest('dist/css'));
-});
+}
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
-});
-
-// Watch Files For Changes
-gulp.task('watch', function() {
-    // gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
-    gulp.watch('css/*.css', ['css']);
-});
+function watch() {
+    gulp.watch('scss/*.scss', sass);
+    gulp.watch('css/*.css', css);
+}
 
 // Default Task
-gulp.task('default', [ 'sass', 'css', 'scripts', 'watch']);
+exports.watch = watch;
+exports.default = watch;
