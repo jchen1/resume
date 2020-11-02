214672c2c735a3cb61c3e0b38777a42f58e211fd
Jeff Chen <jchen@ladderlife.com>
2020-11-02 14:33:27 -0700

    update resume for 2020

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import _ from 'lodash';
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import _ from "lodash";
 
 export class Projects extends React.PureComponent {
   render() {
     const { projects } = this.props;
+    if (projects.length === 0) {
+      return <></>;
+    }
     return (
       <div id="projects">
         <div className="row">
           <div className="header-pre"></div>
-          <h2><svg className="icon-code icon-fw"><use xlinkHref="#icon-code"></use></svg>&nbsp;Selected Projects</h2>
+          <h2>
+            <svg className="icon-code icon-fw">
+              <use xlinkHref="#icon-code"></use>
+            </svg>
+            &nbsp;Selected Projects
+          </h2>
           <div className="header-post"></div>
         </div>
         <div>
           <div className="row">
             <div className="col-md-12">
               <ul>
-                {_.map(projects, ({name, summary, site}) => {
-                  return <li key={name}><ReactMarkdown source={summary} linkTarget="_blank" className="rendered-markdown" /></li>;
+                {_.map(projects, ({ name, summary, site }) => {
+                  return (
+                    <li key={name}>
+                      <ReactMarkdown
+                        source={summary}
+                        linkTarget="_blank"
+                        className="rendered-markdown"
+                      />
+                    </li>
+                  );
                 })}
               </ul>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
